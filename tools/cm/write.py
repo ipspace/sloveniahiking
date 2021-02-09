63490ea2125005d40b916154afd267c5f5a3e3ce
Ivan Pepelnjak <ip@ipspace.net>
2021-02-09 08:35:08 +0100

    Bugfix: use index.md as default output file name

--- tools/cm/write.py
+++ tools/cm/write.py
@@ -4,13 +4,14 @@
 import yaml
 import os
 
-def create_output_file(data=None,target_path=None):
+def create_output_file(data=None,target_path=None,name=None):
   text = data.get('markdown') or data.get('html')
   data.pop('markdown',None)
   data.pop('html',None)
   if not os.path.exists(target_path):
     os.makedirs(target_path)
-  with open("%s/%s.md" % ( target_path,data['name'] ),"wt") as output:
+  fname = name or data.get('name','index')
+  with open("%s/%s.md" % ( target_path,fname ),"wt") as output:
     output.write('---\n')
     output.write(yaml.dump(data,allow_unicode=True))
     output.write('---\n')
