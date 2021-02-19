2ee28b7c8616767f970c6f3c780576f01d11bc16
Ivan Pepelnjak <ip@ipspace.net>
2021-02-19 21:14:40 +0100

    Fix "create stub" script to ignore missing entries and try various lookups to get multipath entries (subdir, prefix)

--- tools/create_hike_stub.py
+++ tools/create_hike_stub.py
@@ -26,23 +26,33 @@ def is_a_page(target):
 def create_stub(path):
   target = path.replace("index.md","index.en.md")
   if is_a_page(target):
-    print("%s is a full-blown page, skipping" % target)
+#    print("%s is a full-blown page, skipping" % target)
     return
 
   if is_a_page(target.replace("_","")):
     print("%s has an alternate full-blown page, skipping" % target)
     return
 
-  print("Creating stub for %s from %s" % (target,path))
+#  print("Creating stub for %s from %s" % (target,path))
   page = cm.read.page(path)
 
   for k in ('markdown','draft','dirty','description'):
     page.pop(k,None)
 
-  ExDirectory = page['name']
+  ExDirectory = page.get('name')
+  if not ExDirectory:
+    print("Cannot figure out the hike name in %s" % path)
+    return
+  
   data = ExData.get(ExDirectory,None)
   if not(data) and '/' in ExDirectory:
-    data = ExSubDir.get(ExDirectory.split('/')[1],None)
+    subdir = ExDirectory.split('/')[1]
+    data = ExSubDir.get(subdir,None)
+
+  if not(data):
+    lookup = ExDirectory.split('/')[0]+"/?pfx="+subdir
+    print("... trying alternate lookup key %s" % lookup)
+    data = ExData.get(lookup)
 
   if not(data):
     print("... cannot find original data for %s" % ExDirectory)
