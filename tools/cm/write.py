a955712ffaeb441bc1cacc4fe816e4a19e231915
Ivan Pepelnjak <ip@ipspace.net>
2021-02-20 20:48:35 +0100

    Add file_path parameter to cm.write.create_output_file to get around the stupidities of parsing known filename into components

--- tools/cm/write.py
+++ tools/cm/write.py
@@ -4,16 +4,19 @@
 import yaml
 import os
 
-def create_output_file(data=None,target_path=None,name=None):
+def create_output_file(data=None,target_path=None,name=None,file_path=None):
   text = data.get('markdown') or data.get('html')
   data.pop('markdown',None)
   data.pop('html',None)
+  if file_path:
+    target_path = os.path.dirname(file_path)
   if not os.path.exists(target_path):
     os.makedirs(target_path)
-  fname = name or data.get('name','index')
-  path = "%s/%s.md" % ( target_path,fname )
-  with open(path,"wt") as output:
-    print(".. Creating output file %s" % path)
+  if not file_path:
+    fname = name or data.get('name','index')
+    file_path = "%s/%s.md" % ( target_path,fname )
+  with open(file_path,"wt") as output:
+    print(".. Creating output file %s" % file_path)
     output.write('---\n')
     output.write(yaml.dump(data,allow_unicode=True))
     output.write('---\n')
