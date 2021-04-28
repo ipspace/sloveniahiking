07ad3025bd5605e4ceb95538f2fb0feed15a98ff
Ivan Pepelnjak <ip@ipspace.net>
2021-04-28 16:39:47 +0200

    Migrate to OpenLayer 6.5.0

--- static/js/map.js
+++ static/js/map.js
@@ -10,60 +10,103 @@ function removeMapPopup() {
   $(".map__popup").remove();
 }
 
-function processHikeData(json,map,l_marker,icon,lang) {
+function enableFeatureClicks(map) {
+  map.on("singleclick",function(event) {
+    removeMapPopup();
+    flist = map.getFeaturesAtPixel(event.pixel);
+    if (flist.length) {
+      marker = flist[0];
+      evt = event.originalEvent;
+      popup = $("<div class='map__popup'>"+
+                "<a href='"+marker.url+"'>"+
+                marker.title+
+                "</a></div>")
+      popup.css({ top: evt.clientY+window.scrollY, left: evt.clientX+window.scrollX, position: "absolute","z-index": 9999 });
+      popup.appendTo(document.body);
+      if (evt.clientX > window.innerWidth / 2) {
+        popup.css("left",evt.clientX+window.scrollX - popup.width() - 12);
+      }
+    }
+  });
+}
+
+/*  icon.red = new OpenLayers.Icon('/images/RedBullet.png',size,offset);
+  icon.green = new OpenLayers.Icon('/images/GreenBullet.png',size,offset);
+  icon.purple = new OpenLayers.Icon('/images/PurpleBullet.png',size,offset); */
+
+function createIcons() {
+  icon = {}
+  icon.peak = new ol.style.Style({
+    image: new ol.style.Icon({
+      opacity: 1,
+      scale: 0.20,
+      src: '/images/RedBullet.png'
+    })
+  })
+  icon.start = new ol.style.Style({
+    image: new ol.style.Icon({
+      opacity: 1,
+      scale: 0.15,
+      src: '/images/RedBullet.png'
+    })
+  })
+  return icon;
+}
+
+function processHikeData(json,map,lang) {
+  points = []
+  icon = createIcons();
+
   for (const hike_data of json) {
     if (hike_data.start || hike_data.peak) {
       var loc = hike_data.multipath ? hike_data.start : hike_data.peak
       loc ||= hike_data.peak
       var latlon = loc.split(',')
-
-      var hikeicon = hike_data.multipath ? icon.purple : icon.red;
-      var hike = new OpenLayers.Marker(getLonLat(latlon[1],latlon[0],map),hikeicon.clone());
-      hike.title = hike_data.title[lang || 'sl'];
-      hike.url = (lang ? "/" + lang : "") + "/hikes/" + hike_data.name.toLowerCase()
-      hike.events.register('mousedown', hike, function(evt) {
-        removeMapPopup();
-        popup = $("<div class='map__popup'>"+
-                  "<a href='"+this.url+"'>"+
-                  this.title+
-                  "</a></div>")
-        popup.css({ top: evt.clientY+window.scrollY, left: evt.clientX+window.scrollX, position: "absolute","z-index": 9999 });
-        popup.appendTo(document.body);
-        if (evt.clientX > window.innerWidth / 2) {
-          popup.css("left",evt.clientX+window.scrollX - popup.width() - 12);
-        }
-        OpenLayers.Event.stop(evt); });
-      l_marker.addMarker(hike);
+      point = new ol.Feature({
+                geometry: new ol.geom.Point(ol.proj.fromLonLat([latlon[1],latlon[0]])),
+              });
+      point.title = hike_data.title[lang || 'sl'];
+      point.url   = (lang ? "/" + lang : "") + "/hikes/" + hike_data.name.toLowerCase();
+      point.icon  = hike_data.multipath ? icon.start : icon.peak;
+      points.push(point)
     }
   }
+  var layer = new ol.layer.Vector({
+                source: new ol.source.Vector({
+                  features: points
+                }),
+                style: function(feature,resolution) {
+                  return feature.icon
+                }
+              });
+  map.addLayer(layer);
+  enableFeatureClicks(map);
 }
 
 function createMap(divname,lat,lon,zoom,lang) {
-  map = new OpenLayers.Map(divname);
-/*  map.addLayer(new OpenLayers.Layer.OSM()); */
+  v = new ol.View({
+    center: ol.proj.fromLonLat([parseFloat(lon),parseFloat(lat)]),
+    zoom: zoom});
 
-  osm = new OpenLayers.Layer.OSM(
-    "OpenStreetMap",
-    // Official OSM tileset as protocol-independent URLs
-    [
-        '//a.tile.openstreetmap.org/${z}/${x}/${y}.png',
-        '//b.tile.openstreetmap.org/${z}/${x}/${y}.png',
-        '//c.tile.openstreetmap.org/${z}/${x}/${y}.png'
-    ],
-    null);
-  map.addLayer(osm);
+  map = new ol.Map({
+    layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM(),
+    }) ],
+  target: divname,
+  view: new ol.View({
+    center: ol.proj.fromLonLat([parseFloat(lon),parseFloat(lat)]),
+    zoom: zoom})
+  });
 
+  $.getJSON('/data/hikes.json',function(data) {
+    processHikeData(data,map,lang)
+  });
+  return;
+
+  map.addLayer(layer);
   var markers = new OpenLayers.Layer.Markers( "Markers" );
   map.addLayer(markers);
-  var size = new OpenLayers.Size(12,12);
-  var offset = new OpenLayers.Pixel(-(size.w/2), -size.h/2);
-  var icon = {};
-  icon.red = new OpenLayers.Icon('/images/RedBullet.png',size,offset);
-  icon.green = new OpenLayers.Icon('/images/GreenBullet.png',size,offset);
-  icon.purple = new OpenLayers.Icon('/images/PurpleBullet.png',size,offset);
-  $.getJSON('/data/hikes.json',function(data) {
-    processHikeData(data,map,markers,icon,lang)
-  });
 
   map.setCenter (getLonLat(lon,lat,map), zoom);
   map.events.register('move',map,removeMapPopup);
