d153deaa587af352910eb131ed2948b35246baf7
Ivan Pepelnjak <ip@ipspace.net>
2021-04-28 19:57:01 +0200

    Add biking tours to regional maps

--- static/js/map.js
+++ static/js/map.js
@@ -50,6 +50,13 @@ function createIcons() {
       src: '/images/RedBullet.png'
     })
   })
+  icon.biking = new ol.style.Style({
+    image: new ol.style.Icon({
+      opacity: 1,
+      scale: 0.20,
+      src: '/images/GreenBullet.png'
+    })
+  })
   return icon;
 }
 
@@ -58,16 +65,26 @@ function processHikeData(json,map,lang) {
   icon = createIcons();
 
   for (const hike_data of json) {
-    if (hike_data.start || hike_data.peak) {
+    if (hike_data.start || hike_data.peak || hike_data.center) {
       var loc = hike_data.multipath ? hike_data.start : hike_data.peak
-      loc ||= hike_data.peak
+      loc ||= hike_data.center || hike_data.start
+      if (hike_data.type == 'biking') {
+        console.log(hike_data)
+      }
       var latlon = loc.split(',')
       point = new ol.Feature({
                 geometry: new ol.geom.Point(ol.proj.fromLonLat([latlon[1],latlon[0]])),
               });
-      point.title = hike_data.title[lang || 'sl'];
-      point.url   = (lang ? "/" + lang : "") + "/hikes/" + hike_data.name.toLowerCase();
-      point.icon  = hike_data.multipath ? icon.start : icon.peak;
+
+      if (hike_data.type == 'biking') {
+        point.title = hike_data.title['en'];
+        point.url   = "/en/biking/" + hike_data.name.toLowerCase();
+        point.icon  = icon.biking;
+      } else {
+        point.title = hike_data.title[lang || 'sl'];
+        point.url   = (lang ? "/" + lang : "") + "/hikes/" + hike_data.name.toLowerCase();
+        point.icon  = hike_data.multipath ? icon.start : icon.peak;
+      }
       points.push(point)
     }
   }
@@ -83,38 +100,44 @@ function processHikeData(json,map,lang) {
   enableFeatureClicks(map);
 }
 
-function createMap(divname,lat,lon,zoom,lang) {
+function createMap(divname,lat,lon,zoom,gpx) {
   v = new ol.View({
     center: ol.proj.fromLonLat([parseFloat(lon),parseFloat(lat)]),
     zoom: zoom});
 
+  var gpx_layer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      url: gpx,
+      format: new ol.format.GPX(),
+    }),
+    style: new ol.style.Style({
+      stroke: new ol.style.Stroke({
+        color: "#f00",
+        width: 2
+      })
+    })
+  });
+
   map = new ol.Map({
     layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM(),
-    }) ],
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+      }),
+      gpx_layer ],
   target: divname,
   view: new ol.View({
     center: ol.proj.fromLonLat([parseFloat(lon),parseFloat(lat)]),
     zoom: zoom})
   });
-
-  $.getJSON('/data/hikes.json',function(data) {
-    processHikeData(data,map,lang)
-  });
-  return;
-
-  map.addLayer(layer);
-  var markers = new OpenLayers.Layer.Markers( "Markers" );
-  map.addLayer(markers);
-
-  map.setCenter (getLonLat(lon,lat,map), zoom);
-  map.events.register('move',map,removeMapPopup);
 }
 
 $(function() {
   map = $("#mapdiv")
   if (map.length) {
     createMap("mapdiv",map.attr("data-lat"),map.attr("data-lon"),map.attr("data-zoom"),map.attr("data-lang"))
+  }
+  map = $("#mapgpx")
+  if (map.length) {
+    createMap("mapgpx",map.attr("data-lat"),map.attr("data-lon"),map.attr("data-zoom"),map.attr("data-gpx"))
   }
 });
