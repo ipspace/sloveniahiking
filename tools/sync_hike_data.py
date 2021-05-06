d94a24e7fe7dd17fa3a6a853db53da9650291d6d
Ivan Pepelnjak <ip@ipspace.net>
2021-05-06 09:21:30 +0200

    Starting point support

--- tools/sync_hike_data.py
+++ tools/sync_hike_data.py
@@ -32,7 +32,7 @@ def sync_hike_data(si_path):
   en_page = cm.read.page(en_path)
   en_yaml = yaml.dump(en_page)
 
-  for key in ('delta','duration','height','lead','multilead','multipath','maplink','start','peak','video','region'):
+  for key in ('delta','duration','height','lead','multilead','multipath','maplink','start','startpoint','peak','video','region'):
     si_val = si_page.get(key)
     en_val = en_page.get(key)
     if not si_val and not en_val:
